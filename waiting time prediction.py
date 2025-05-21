a15dd39e867a46b4863a8e1c81ba3658dde22d29
Rui Tang <76582145+yiwangxi-tech@users.noreply.github.com>
2025-05-21 20:10:41 +0800

    Waiting time prediction.py

--- waiting time prediction.py
+++ waiting time prediction.py
@@ -11,43 +11,43 @@ from sklearn.svm import SVR
 from scipy.stats import uniform, randint
 from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 
-# 读取 Excel 文件，以1F咽拭子场景为例
+# Read the Excel file, the 1F throat swab cohort was used as an example
 df = pd.read_excel('1F咽拭子.xlsx')
 
-# 如果是影像检查，则将‘分诊签到时间’替换为‘签到/登记时间’
+# If it is a radiology examination, replace "分诊签到时间" with "签到/登记时间"
 df_rd = df.drop_duplicates(subset=['病人ID', 'Visit times', '分诊签到时间'], keep='first')
 
-# 提取时间信息并将该列转换为日期时间类型，如果是影像检查，则将‘分诊签到时间’替换为‘签到/登记时间’，将‘采样时间’替换为‘上机检查时间’
+# Extract the time information and convert this column to the datetime type. If it is a radiology examination, replace "分诊签到时间" with "签到/登记时间"，replace "采样时间" with "上机检查时间"
 df_rd['分诊签到时间'] = pd.to_datetime(df_rd['分诊签到时间'])
 df_rd['采样时间'] = pd.to_datetime(df_rd['采样时间'])
 
-# 按照时间先后顺序整理Excel文件
+# Sort the Excel file in chronological order
 df_rd_rank = df_rd.sort_values(by='分诊签到时间', ascending=True)
 
-# 提取月、日、时信息并添加到新的列中
+# Extract the month, day and time information and add it to a new column
 df_rd_rank['month'] = df_rd_rank[time_column].dt.month
 df_rd_rank['day'] = df_rd_rank[time_column].dt.day
 df_rd_rank['hour'] = df_rd_rank[time_column].dt.hour
 df_rd_rank['week'] = df_rd_rank[time_column].dt.weekday
 
-# 按照“month”、“day”、“hour”进行统计每小时达到的人数
+# Count the arrival rate, based on "month", "day" and "hour"
 grouped = df_rd_rank.groupby(['month', 'day', 'hour']).size().reset_index(name='arrival rate')
 
-# 计算排队人数，如果是影像检查，则将‘分诊签到时间’替换为‘签到/登记时间’，将‘采样时间’替换为‘上机检查时间’
+# Calculate the number of queuing patient，If it is a radiology examination, replace "分诊签到时间" with "签到/登记时间"，replace "采样时间" with "上机检查时间"
 for i in range(len(df_rd_rank)):
     current_sign_in_time = df_rd_rank.loc[i, '分诊签到时间']
-    start_index = max(0, i - 10)   # 需动态调整，在10-100之间，根据队列场景的拥挤程度
+    start_index = max(0, i - 10)   # It needs to be dynamically adjusted, ranging from 10 to 100, depending on the congestion level of the queue scene
     count = (df_rd_rank.loc[start_index:i-1, '采样时间'] > current_sign_in_time).sum()
     df_rd_rank.loc[i, 'the number of queuing patient'] = count
 
-# 预处理数据
+# Data preprocessing
 y = df["waiting time"]
 X = df[["month", "day", "hour", "week", "the number of queuing patient", "arrival rate"]]
 
-# 划分数据集
+# dataset spliting
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-# 随机森林
+# RandomForest
 rf = RandomForestRegressor()
 rf.fit(X_train, y_train)
 rf_predictions = rf.predict(X_test)
@@ -77,7 +77,7 @@ lr_reg = LinearRegression()
 lr_reg.fit(X_train, y_train)
 lr_reg_predictions = lr_reg.predict(X_test)
 
-# 弹性网络回归
+# ElasticNet
 en_reg = ElasticNet()
 en_reg.fit(X_train, y_train)
 en_reg_predictions = en_reg.predict(X_test)
@@ -87,7 +87,7 @@ cart_reg = DecisionTreeRegressor()
 cart_reg.fit(X_train, y_train)
 cart_reg_predictions = cart_reg.predict(X_test)
 
-# 袋装法回归（Bagging）
+# BaggingRegressor
 bagging_reg = BaggingRegressor()
 bagging_reg.fit(X_train, y_train)
 bagging_reg_predictions = bagging_reg.predict(X_test)
@@ -97,7 +97,7 @@ lgb_reg = lgb.LGBMRegressor()
 lgb_reg.fit(X_train, y_train)
 lgb_reg_predictions = lgb_reg.predict(X_test)
 
-# 评估模型
+# Model evulation
 def evaluate_model(y_test, predictions, model_name):
     mae = mean_absolute_error(y_test, predictions)
     mse = mean_squared_error(y_test, predictions)
