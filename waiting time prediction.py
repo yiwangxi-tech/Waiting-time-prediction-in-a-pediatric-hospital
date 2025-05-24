52cae379af65b2125e8b5b308c9c58143c4514d8
Rui Tang <76582145+yiwangxi-tech@users.noreply.github.com>
2025-05-24 11:17:47 +0800

    waiting time prediction.py

--- waiting time prediction.py
+++ waiting time prediction.py
@@ -62,6 +62,7 @@ X = df_rd_rank_denosie[["month", "day", "hour", "week", "the number of queuing p
 # dataset spliting
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
+# Preliminary evaluation of different models
 # RandomForest
 rf = RandomForestRegressor()
 rf.fit(X_train, y_train)
